f153695c6c21e09b536180ae38ba97c39cef6cc7
Alexander Martinez Millan <alexander.marti.mil@gmail.com>
2022-08-15 18:05:19 -0500

    version 3.0

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
+
+
+// por si se quiere subir tambien a github pages:
+
+/* 
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: './docs'
+  }
+})
+*/
+
+// https://www.udemy.com/course/react-cero-experto/learn/lecture/19762390#questions/17691714
+
+// y 
+
+// https://www.udemy.com/course/react-cero-experto/learn/lecture/32018146#questions/17691714
